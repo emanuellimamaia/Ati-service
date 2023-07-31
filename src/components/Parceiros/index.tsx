a21e94d085e3ce26c2c03e23be06bb42369ec7f4
emanuel maia <emanuellimamaia@gmail.com>
2023-07-31 17:11:01 -0400

    att dia 31

--- src/components/Parceiros/index.tsx
+++ src/components/Parceiros/index.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
-import Logo from "/public/images/logo.svg"
 import Sonda from "/public/images/empresa/sonda.jpeg"
 import am from "/public/images/empresa/3am.jpeg"
 import contabilidade from "/public/images/empresa/contabilidade.jpeg"
-import emsa from "/public/images/empresa/emsa.jpeg"
-import global from "/public/images/empresa/global.jpeg"
+import emsa from "/public/images/empresa/emsa.svg"
+import global from "/public/images/empresa/global.svg"
 import fenix from "/public/images/empresa/fenix.jpeg"
-import ioam from "/public/images/empresa/ioam.jpeg"
+import ioam from "/public/images/empresa/iom.svg"
 import mega from "/public/images/empresa/mega.jpeg"
-import rb from "/public/images/empresa/rb.jpeg"
+import rb from "/public/images/empresa/rb.svg"
 import vale from "/public/images/empresa/vale.jpeg"
 
 
@@ -16,56 +15,57 @@ import vale from "/public/images/empresa/vale.jpeg"
 export default function Parceiros() {
   return (
 
-    <div className="flex flex-col md:flex-row py-8 justify-center items-center">
-      <div className="w-full mb-8 font-semibold md:ml-20 text-4xl border-l-8 border-red-700 ">
-        Parceiros
+    <div className="flex flex-col lg:flex-row py-8 justify-center items-center">
+      <div className="w-1/3 mb-8  font-semibold md:ml-10 max-h-40 text-4xl border-l-8 border-red-700 ">
+        Clientes
       </div>
-      <div className=" h-full w-full flex flex-row">
-        <div className="flex flex-col ">
+      <div className=" h-full w-auto flex flex-row ">
+        <div className="flex flex-col lg:flex-row">
           <div className="flex flex-row">
 
             <Image
-              className="h-20 w-20"
+              className="p-4 h-24 w-24 "
 
               src={Sonda}
               alt="blog" />
             <Image
-              className="h-20 w-20"
-              src={am}
+              className="py-4 h-24 w-24 "
+              src={rb}
               alt="blog" />
             <Image
-              className="h-20 w-20"
+              className="p-4 h-24 w-24 "
               src={contabilidade}
               alt="blog" />
             <Image
-              className="h-20 w-20"
+              className="p-4 h-24 w-24 "
               src={emsa}
               alt="blog" />
             <Image
-              className="h-20 w-20"
+              className="p-4 h-24 w-24 "
               src={vale}
               alt="blog" />
           </div>
-          <div className="flex flex-row pt-4">
+          <div className="p-4 flex flex-row pt-4">
             <Image
-              className="h-20 w-20"
+              className="p-4  h-24 w-24 "
               src={mega}
               alt="blog" />
             <Image
-              className="h-20 w-20"
+              className="p-4 h-24 w-24 "
               src={fenix}
               alt="blog" />
             <Image
-              className="h-20 w-20"
+              className="py-4 h-24 w-24 m-0 "
               src={global}
               alt="blog" />
             <Image
-              className="h-20 w-20"
+              className="p-4 h-24 w-24 "
               src={ioam}
               alt="blog" />
-            <Image
-              className="h-20 w-20"
-              src={rb}
+           
+                <Image
+              className=" p-4 h-24 w-24 "
+              src={am}
               alt="blog" />
 
           </div>
